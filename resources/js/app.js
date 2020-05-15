d7c85eec8dace44611447971f82e09b339cf0665
Dedi Triyadi <dedi.triyadi.barnawan@gmail.com>
2020-05-15 12:38:53 +0700

    edit home page laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import './bootstrap'
+import Vue from 'vue'
+import App from './components/ExampleComponent.vue'
+
+const vm = new Vue({
+	el: '#app',
+	render: h => h(App)
+
+})
